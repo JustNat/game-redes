4e1c84aae3ae54bdc849ee0af61a3d1fd558ef2d
Gabriel Bruce <gabrielcp4@outlook.com>
2025-07-04 17:02:11 -0300

    latency resolver

--- client_udp.py
+++ client_udp.py
@@ -1,8 +1,9 @@
 import socket
 import pygame
-import pickle
+import struct
+import threading
 
-WIDTH, HEIGHT = 600, 400
+WIDTH, HEIGHT = 1200, 800
 PADDLE_WIDTH, PADDLE_HEIGHT = 10, 60
 BALL_SIZE = 10
 
@@ -10,14 +11,32 @@ pygame.init()
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
-host_y = HEIGHT // 2
-client_y = HEIGHT // 2
-ball_x, ball_y = WIDTH // 2, HEIGHT // 2
+host_ip = "192.168.1.42"  # IP do host
+host_port = 5555
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-host_ip = "192.168.1.42"  # 🔁 Substitua pelo IP real do host
-sock.connect((host_ip, 5555))
 sock.setblocking(False)
+
+client_y = HEIGHT // 2
+host_y = HEIGHT // 2
+ball_x, ball_y = WIDTH // 2, HEIGHT // 2
+
+def listen_host():
+    global host_y, ball_x, ball_y
+    while True:
+        try:
+            data, addr = sock.recvfrom(1024)
+            if data:
+                host_y_recv, ball_x_recv, ball_y_recv = struct.unpack('i ff', data)
+                host_y = host_y_recv
+                ball_x = ball_x_recv
+                ball_y = ball_y_recv
+        except BlockingIOError:
+            pass
+        except Exception as e:
+            print("Erro recv:", e)
+
+threading.Thread(target=listen_host, daemon=True).start()
 
 def draw():
     win.fill((0, 0, 0))
@@ -27,7 +46,6 @@ def draw():
     pygame.display.flip()
 
 run = True
-
 while run:
     clock.tick(60)
 
@@ -41,16 +59,12 @@ while run:
     if keys[pygame.K_DOWN] and client_y < HEIGHT - PADDLE_HEIGHT:
         client_y += 5
 
+    # Envia posição paddle para host (int)
+    packet = struct.pack('i', client_y)
     try:
-        sock.send(pickle.dumps(client_y))
-    except:
-        pass
-
-    try:
-        data = sock.recv(1024)
-        host_y, ball_x, ball_y = pickle.loads(data)
-    except BlockingIOError:
-        pass
+        sock.sendto(packet, (host_ip, host_port))
+    except Exception as e:
+        print("Erro sendto:", e)
 
     draw()
 
--- host_udp.py
+++ host_udp.py
@@ -1,10 +1,12 @@
 import socket
+import threading
 import pygame
-import pickle
+import struct
 
 WIDTH, HEIGHT = 1200, 800
 PADDLE_WIDTH, PADDLE_HEIGHT = 10, 60
 BALL_SIZE = 10
+BALL_SPEED = 4
 
 pygame.init()
 win = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -12,12 +14,34 @@ clock = pygame.time.Clock()
 
 host_y = HEIGHT // 2
 client_y = HEIGHT // 2
-ball_x, ball_y = WIDTH // 2, HEIGHT // 2
-ball_dx, ball_dy = -4, 4
+
+def reset_ball():
+    return WIDTH // 2, HEIGHT // 2, -BALL_SPEED, BALL_SPEED
+
+ball_x, ball_y, ball_dx, ball_dy = reset_ball()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("0.0.0.0", 5555))
+sock.setblocking(False)
+
 client_addr = None
+
+def listen_client():
+    global client_y, client_addr
+    while True:
+        try:
+            data, addr = sock.recvfrom(1024)
+            if data:
+                # client envia posição do paddle (int)
+                (client_y_recv,) = struct.unpack('i', data)
+                client_y = client_y_recv
+                client_addr = addr
+        except BlockingIOError:
+            pass
+        except Exception as e:
+            print("Erro no recv:", e)
+
+threading.Thread(target=listen_client, daemon=True).start()
 
 def draw():
     win.fill((0, 0, 0))
@@ -27,17 +51,8 @@ def draw():
     pygame.display.flip()
 
 run = True
-sock.setblocking(False)
-
 while run:
     clock.tick(60)
-
-    try:
-        data, addr = sock.recvfrom(1024)
-        client_y = pickle.loads(data)
-        client_addr = addr
-    except BlockingIOError:
-        pass
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -49,20 +64,33 @@ while run:
     if keys[pygame.K_s] and host_y < HEIGHT - PADDLE_HEIGHT:
         host_y += 5
 
+    # Atualiza bola
     ball_x += ball_dx
     ball_y += ball_dy
 
+    # Colisão com teto e chão
     if ball_y <= 0 or ball_y >= HEIGHT - BALL_SIZE:
         ball_dy *= -1
 
-    if ball_x <= 30 and host_y < ball_y < host_y + PADDLE_HEIGHT:
-        ball_dx *= -1
-    if ball_x >= WIDTH - 40 and client_y < ball_y < client_y + PADDLE_HEIGHT:
+    # Colisão com host paddle
+    if ball_x <= 30 + PADDLE_WIDTH and host_y <= ball_y <= host_y + PADDLE_HEIGHT:
         ball_dx *= -1
 
+    # Colisão com client paddle
+    if ball_x + BALL_SIZE >= WIDTH - 40 and client_y <= ball_y <= client_y + PADDLE_HEIGHT:
+        ball_dx *= -1
+
+    # Reinicia bola se passar das bordas
+    if ball_x < 0 or ball_x > WIDTH:
+        ball_x, ball_y, ball_dx, ball_dy = reset_ball()
+
+    # Envia estado para client: host_y (int), ball_x (float), ball_y (float)
     if client_addr:
-        game_state = pickle.dumps((host_y, ball_x, ball_y))
-        sock.sendto(game_state, client_addr)
+        packet = struct.pack('i ff', host_y, ball_x, ball_y)
+        try:
+            sock.sendto(packet, client_addr)
+        except Exception as e:
+            print("Erro no sendto:", e)
 
     draw()
 
